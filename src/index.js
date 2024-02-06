19ddb6f6b9982cb7d1deadaa621a4d34b8407310
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-02-06 16:49:16 +0400

    add button

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-// создаем элемент заголовка
-const heading = document.createElement('h1')
-heading.textContent = 'Как интересно!'
-
-// добавляем заголовок в DOM
-const root = document.querySelector('#root')
-root.append(heading)
