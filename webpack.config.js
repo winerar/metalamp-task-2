ef41a616fce25f0af4f2d6d0c06715ae9e4f6f5b
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-02-06 16:48:54 +0400

    configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,37 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const pug = {
+  test: /\.pug$/,
+  use: ['html-loader', 'pug-html-loader']
+}
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  entry: {
-    main: './index.js',
-  },
+  mode: 'development',
+  entry: './app.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].[contenthash].js',
   },
   module: {
     rules: [
-      // CSS, PostCSS, Sass
+      pug,
       {
-        test: /\.s[ac]ss$/,
-        use: ['sass-loader'],
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader',],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'webpack Boilerplate',
-      template: path.resolve(__dirname, './src/template.html'), // шаблон
+      template: path.resolve(__dirname, './src/index.pug'), // шаблон
       filename: 'index.html', // название выходного файла
+      // inject: false,
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
     }),
     new CleanWebpackPlugin(),
   ],
